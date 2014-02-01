a920d69fa49497b58e1f5314bb08f5156127c090
rbarry825 <rbarry825@gmail.com>
2014-02-01 12:45:25 -0500

    Levi - deleted last line

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 		System.out.println("Transpiration Rate Simulator Project!!");
         System.out.println("I'm so excited!");
         System.out.println("Zhen");
-        //Test
         System.out.println("Let's do it!");
-        System.out.println("This is a change");
 
 	}
 
