07ddab94eef5c48488a61a4b805e8741efac334a
Jack Coy <jackman3000@gmail.com>
2014-02-01 13:16:04 -0500

    Jack - Commit Practice

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Transpiration Rate Simulator Project!!");
-        System.out.println("I'm so excited!");
-        System.out.println("Zhen");
-        System.out.println("Let's do it!");
-        System.out.println("");
+		System.out.println("I'm so excited!");
+		System.out.println("Zhen");
+		System.out.println("Let's do it!");
+		System.out.println("Hello this is me Dur");
 	}
 }
