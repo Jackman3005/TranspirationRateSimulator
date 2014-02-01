41d02a36ddf04618ac6acdbe3c76e7550b0c909b
rbarry825 <rbarry825@gmail.com>
2014-02-01 12:12:39 -0500

    Levi code test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Transpiration Rate Simulator Project!!");
+        System.out.println("I'm so excited!");
 	}
 
 }
