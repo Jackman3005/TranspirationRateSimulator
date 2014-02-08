d4b0d7b655f526cd9f02a2394677f6370d0165e1
Jack Coy <jackman3000@gmail.com>
2014-02-08 13:54:52 -0500

    Jack - Re-added the project in a folder one level deeper and added the test project.

---
+++ TranspirationRateSimulator/README.md
@@ -0,0 +1,4 @@
+TranspirationRateSimulator
+==========================
+
+This software allows the user to enter factors that will affect the rate of transpiration for a single leaf on a plant. The program will then predict the rate of water loss for the leaf.
---
+++ TranspirationRateSimulator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Transpiration Rate Simulator Project!!");
+		System.out.println("I'm so excited!");
+		System.out.println("Zhen");
+		System.out.println("Let's do it!");
+		System.out.println("Hello this is Levi Dur");
+
+
+	}
+}
