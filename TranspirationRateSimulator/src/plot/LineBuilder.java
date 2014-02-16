cde5a7311d7ace95f2b30815048f09bbafbb77b4
rbarry825 <rbarry825@gmail.com>
2014-02-16 16:44:34 -0500

    Levi - WaterLossAlgorithm calculates waterloss from all parameters; Pair - holds the x- and y- values to construct the line; LineBuilder - create this object to build the line, then call getLineArray to return an Arraylist of the ordered pairs of the line.

--- TranspirationRateSimulator/src/plot/LineBuilder.java
+++ TranspirationRateSimulator/src/plot/LineBuilder.java
@@ -1,49 +1,98 @@
 package plot;
 
+import java.util.ArrayList;
+
 /**
  * @author: Levi
- * @version: 2/15/14
+ * @version: 2/16/14
  */
 public class LineBuilder {
-    private double dwv, ast, gst, gias, gleaf, gbl, gttal, cias, cair, dc, jwv, wl, cwv;
-    private static final double DIAS = 6 * Math.pow(10, -5);
-    private static final double GC = 0.001;
-    private static final double HIAS = 1.0;
-    private static final double MP = 0.4470;
-    private double waterloss;
-    private SimulationParameters indVariable;
+    private static final int PIXEL_RANGE = 200;
+    private double increment;
+    private ArrayList<Pair> lineArray = new ArrayList<Pair>();
+    private int min;
+    private int max;
+    private final double leafWidth;
+    private final double leafArea;
+    private final double stomaDensity;
+    private final double stomaRadius;
+    private final double stomaDepth;
+    private final double temperature;
+    private final double humidity;
+    private final double windspeed;
 
-    public LineBuilder(SimulationParameters indVariable, double leafWidth, double leafArea, double stomaDensity,
-                       double stomaRadius, double stomaDepth, double temperature, double humidity, double windspeed) {
+    public LineBuilder (SimulationParameters indVariable, int min, int max, double leafWidth, double leafArea, double stomaDensity,
+                        double stomaRadius, double stomaDepth, double temperature, double humidity, double windspeed){
+        this.min = min;
+        this.max = max;
+        this.leafWidth = leafWidth;
+        this.leafArea = leafArea;
+        this.stomaDensity = stomaDensity;
+        this.stomaRadius = stomaRadius;
+        this.stomaDepth = stomaDepth;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.windspeed = windspeed;
+        this.increment = ((double) (max - min)) / PIXEL_RANGE;
 
-        this.indVariable = indVariable;
-
-        // These only rely on user input for calculations
-        this.dwv = (2.13*(10^-5))*(Math.pow((temperature+273)/273,1.8));
-        this.ast = Math.PI*(Math.pow(Math.pow(stomaRadius*10,-6),2));
-        this.cwv = 2.848 + (0.1082 * temperature) + ((3.283 * Math.pow(10, -4)) * Math.pow(temperature, 2))
-                + ((2.06 * Math.pow(10, -5)) * Math.pow(temperature, 3));
-        this.gbl = dwv /(4*Math.pow((leafWidth/100)/((windspeed+0.00001) * MP),0.5));
-
-        // These rely on the above
-        this.gst = (dwv * (stomaDensity * Math.pow(10, 6)) * ast) / ((stomaDepth * Math.pow(10, -6))
-                + (stomaRadius * Math.pow(10, -6)));
-        this.cias = cwv * HIAS;
-        this.cair = (humidity / 100) * cwv;
-        this.gias = dwv / DIAS;
-
-        // These rely on each other, in order
-        this.gleaf = ((gias * gst)/(gias + gst)) + GC;
-        this.gttal = (gleaf * gbl) / (gleaf + gbl);
-        this.dc = cias - cair;
-        this.jwv = gttal * dc * 18;
-        this.wl = (jwv * (leafArea * Math.pow(10,-4)) * 3.6 * Math.pow(10,3));
-
-        // Waterloss for 100-leaf plant
-        this.waterloss = 100 * wl;
-
+        populateArray(indVariable);
     }
 
+    private void populateArray(SimulationParameters indVariable){
+        switch (indVariable){
+            case LeafWidth:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(i, leafArea, stomaDensity, stomaRadius, stomaDepth,
+                            temperature, humidity, windspeed)));
+                }
+                break;
+            case LeafArea:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, i, stomaDensity, stomaRadius, stomaDepth,
+                            temperature, humidity, windspeed)));
+                }
+                break;
+            case StomaDensity:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, i, stomaRadius, stomaDepth,
+                            temperature, humidity, windspeed)));
+                }
+                break;
+            case StomaRadius:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, stomaDensity, i, stomaDepth,
+                            temperature, humidity, windspeed)));
+                }
+                break;
+            case StomaDepth:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, stomaDensity, stomaRadius, i,
+                            temperature, humidity, windspeed)));
+                }
+                break;
+            case Temperature:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, stomaDensity, stomaRadius, stomaDepth,
+                            i, humidity, windspeed)));
+                }
+                break;
+            case RelativeHumidityOfAir:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, stomaDensity, stomaRadius, stomaDepth,
+                            temperature, i, windspeed)));
+                }
+                break;
+            case WindSpeed:
+                for (double i = min; i < max; i += increment) {
+                    this.lineArray.add(new Pair(i, WaterLossAlgorithm.getWaterLoss(leafWidth, leafArea, stomaDensity, stomaRadius, stomaDepth,
+                            temperature, humidity, i)));
+                }
+                break;
+        }
+    }
 
-
+    //Getter
+    public ArrayList<Pair> getLineArray(){
+        return lineArray;
+    }
 }
---
+++ TranspirationRateSimulator/src/plot/Pair.java
@@ -0,0 +1,25 @@
+package plot;
+
+/**
+ * @author: Levi
+ * @version: 2/16/14
+ */
+public class Pair {
+    private double xValue;
+    private double yValue;
+
+    public Pair (double indVariable, double depVariable) {
+        this.xValue = indVariable;
+        this.yValue = depVariable;
+    }
+
+    //Getters
+
+    public double getXValue() {
+        return xValue;
+    }
+
+    public double getYValue() {
+        return yValue;
+    }
+}
