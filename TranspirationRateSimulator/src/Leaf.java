b7c5c022067b3d2b9fad58bde63bc1e964cf2fcd
Junpeng Li <jli12@emich.edu>
2014-03-26 18:50:04 -0400

    Add pic with animation

---
+++ TranspirationRateSimulator/src/Leaf.java
@@ -0,0 +1,92 @@
+
+
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+/**
+ * @author: Junpeng Li
+ * @version: 3/26/14
+ */
+
+
+
+public class Leaf {
+   
+    private static final int STAR_COUNT = 100;
+    
+    static private final Circle[] nodes = new Circle[STAR_COUNT];
+    private final double[] angles = new double[STAR_COUNT];
+    private final long[] start = new long[STAR_COUNT];
+    
+    private final double[] width = new double[STAR_COUNT];
+    private final double[] height = new double[STAR_COUNT];
+    
+    private final Random random = new Random();
+
+    private int CP_width = 260;
+    private int CP_height = 300;
+    
+	public void Leaf()
+	{
+       
+	}
+	
+	public Pane play(final Stage primaryStage)
+	{		
+	Pane canvas = new Pane();
+
+	Group root = new Group();
+	
+	Group circles = new Group();
+	
+	 for (int i=0; i<STAR_COUNT; i++) {
+            nodes[i] = new Circle(5 , Color.CYAN);
+            angles[i] = 0.7 * Math.PI * random.nextDouble() + Math.PI/6;
+            start[i] = random.nextInt(2000000000);
+            
+            width[i] =  CP_width + (Math.random() - 0.5) * 100;
+            height[i] = CP_height + (Math.random() - 0.5) * 100;
+            
+     	   circles.getChildren().add(nodes[i]);
+        }
+	 root.getChildren().add(circles);
+	 
+	 canvas.getChildren().add(root);
+	 
+       // final Scene scene = new Scene(canvas, 620, 350, Color.BLACK);
+    	//scene.getStylesheets().add(TRS_Frame.class.getResource("application.css").toExternalForm());
+    	
+    	String image = TRS_Frame.class.getResource("leaf.gif").toExternalForm();
+    	canvas.setStyle("-fx-background-image: url('" + image + "'); -fx-background-position: center center;  -fx-background-repeat: no-repeat;");
+        
+       // primaryStage.setScene(scene);
+      //  primaryStage.show();
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+              //  final double width = 0.5 * primaryStage.getWidth();
+                //final double height = 0.2 * primaryStage.getHeight();
+               // final double radius = Math.sqrt(2) * Math.max(0.1*primaryStage.getWidth(),0.1*primaryStage.getHeight());
+                final double radius = 70;
+                for (int i=0; i<STAR_COUNT; i++) {
+                    final Node node = nodes[i];
+                    final double angle = angles[i];
+                    final long t = (now - start[i]) % 2000000000;
+                    final double d = t * radius / 2000000000.0;
+                    node.setTranslateX(Math.cos(angle) * d + width[i]);
+                    node.setTranslateY(Math.sin(angle) * d + height[i]);
+                }
+            }
+        }.start();
+		return canvas;
+	}
+	
+}
--- TranspirationRateSimulator/src/TRS_Frame.java
+++ TranspirationRateSimulator/src/TRS_Frame.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import plot.PlotPanel;
@@ -20,9 +22,15 @@ public class TRS_Frame extends Application {
         stage.setTitle("Transpiration Rate Simulator");
 
         GridPane mainPane = new GridPane();
+        mainPane.getColumnConstraints().add(new ColumnConstraints(600)); 
+        mainPane.getColumnConstraints().add(new ColumnConstraints(500)); 
+        mainPane.getRowConstraints().add(new RowConstraints(450)); 
+        mainPane.getRowConstraints().add(new RowConstraints(500)); 
+        
         mainPane.add(new PlotPanel(new WaterLossGraphModel()), 0, 0);
         mainPane.add(new InputTable(), 1, 0);
         mainPane.add(new TheOutputTable(), 0, 1);
+	    mainPane.add(new Leaf().play(stage),1,1);
         
         Scene mainScene = new Scene(mainPane, 1000, 650, Color.LIGHTBLUE);
         stage.setScene(mainScene);
